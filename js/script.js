68f69a1f90393695ab5fcb079c5c71110932b37c
Ksenia Typel <ksenia.typel@gmail.com>
2022-02-15 23:04:10 +0100

    finished generetaeTags function

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@
   };
 
 
+
   const  generateTitleLinks = function () {
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
@@ -71,28 +72,74 @@
   console.log('Title List after loop:', generateTitleLinks);
 
 
+
   const generateTags = function() {
 
     /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
 
     /* START LOOP: for every article: */
-    /* find tags wrapper */
-    /* make html variable with empty string */
-    /* get tags from data-tags attribute */
-    /* split tags into array */
+    for (let article of articles) {
 
+      /* find tags wrapper */
+      const tagWrapper = article.querySelector(optArticleTagsSelector);
+      console.log('tag Wrapper is:', tagWrapper);
 
-    /* START LOOP: for each tag */
-    /* generate HTML of the link */
-    /* add generated code to html variable */
-    /* END LOOP: for each tag */
+      /* make html variable with empty string */
+      let html = '';
 
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log('Tag is :', articleTags);
 
-    /* insert HTML of all the links into the tags wrapper */
-    /* END LOOP: for every article: */
+      /* split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+      console.log('Array is :', articleTagsArray);
 
+      /* START LOOP: for each tag */
+      for (let tag of articleTagsArray){
+        console.log('Single tag is :', tag);
+
+        /* generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+        console.log('created HTML of the link :', linkHTML);
+
+        /* add generated code to html variable */
+        html = html + linkHTML;
+      }
+
+      /* insert HTML of all the links into the tags wrapper */
+      tagWrapper.innerHTML = html;
+      console.log('Tag Wrapper after loop is:', html);
+    }
   } ;
-
   generateTags();
 
+  const tagClickHandler = function (event){
+
+    /* prevent default action for this event */
+    /* make new constant named "clickedElement" and give it the value of "
+    /* make a new constant "href" and read the attribute "href" of the cli
+    /* make a new constant "tag" and extract tag from the "href" constant
+    /* find all tag links with class active */
+    /* START LOOP: for each active tag link */
+    /* remove class active */
+    /* END LOOP: for each active tag link */
+    /* find all tag links with "href" attribute equal to the "href" consta
+    /* START LOOP: for each found tag link */
+    /* add class active */
+    /* END LOOP: for each found tag link */
+    /* execute function "generateTitleLinks" with article selector as argu*/
+  };
+
+  const addClickListenersToTags = function(){
+    /* find all links to tags */
+    /* START LOOP: for each link */
+    /* add tagClickHandler as event listener for that link */
+    /* END LOOP: for each link */
+  };
+  addClickListenersToTags();
+
 }
+
+
