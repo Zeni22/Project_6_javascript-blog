910a156e3a3ec2e4bf9ffb1831e2231adf65fc29
Ksenia Typel <ksenia.typel@gmail.com>
2022-02-16 20:44:46 +0100

    all comments added, guidance mode

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list';
 
+  //CLASSES
   const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
@@ -13,53 +14,69 @@
     console.log('Link was clicked!');
     console.log(event);
 
+    /*[DONE]remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
     }
 
+    /*[DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
 
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
     for (let activeArticle of activeArticles) {
       activeArticle.classList.remove('active');
     }
 
+    /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log('Clicked element is :', articleSelector);
 
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log('Found article is :', targetArticle);
 
+    /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
     console.log('Added class active to :', clickedElement);
   };
 
 
-
+  //TITLE LINKS
   const  generateTitleLinks = function () {
+
+    /* [DONE] remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
     console.log('Remove contents title list: ', document.querySelector(optTitleListSelector));
 
+    /* [DONE] find all the articles and save them to variable: articles */
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
 
+    /* [DONE] for each article: */
     for (let article of articles) {
+
+      /* [DONE] get the article id */
       const articleId = article.getAttribute('id');
       console.log('Found article is :', articleId);
 
+      /* [DONE] find the title element and  get the title from the title element */
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       console.log('Found article title is :', articleId);
 
+      /* [DONE] create HTML of the link */
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
       console.log('created HTML of the link :', linkHTML);
 
+      /* [DONE] insert link into html variable */
       html = html + linkHTML;
       console.log('HTML after loop:', html);
     }
 
+    /* [DONE] insert link into titleList */
     titleList.innerHTML = html;
 
     const links = document.querySelectorAll('.titles a');
@@ -69,10 +86,9 @@
     }
   };
   generateTitleLinks();
-  console.log('Title List after loop:', generateTitleLinks);
 
 
-
+  //TAGS
   const generateTags = function() {
 
     /* find all articles */
@@ -112,15 +128,22 @@
       tagWrapper.innerHTML = html;
       console.log('Tag Wrapper after loop is:', html);
     }
-  } ;
+  };
   generateTags();
 
   const tagClickHandler = function (event){
 
     /* prevent default action for this event */
-    /* make new constant named "clickedElement" and give it the value of "
-    /* make a new constant "href" and read the attribute "href" of the cli
-    /* make a new constant "tag" and extract tag from the "href" constant
+    event.preventDefault();
+
+    /* make new constant named "clickedElement" and give it the value of "this" */
+    const clickedElement = this;
+
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+
+    /* make a new constant "tag" and extract tag from the "href" constant*/
+
     /* find all tag links with class active */
     /* START LOOP: for each active tag link */
     /* remove class active */
